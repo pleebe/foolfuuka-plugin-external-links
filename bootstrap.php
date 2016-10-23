8e7cd37c83c618657fe2c659f120e1e2355c30db
pleebe <pleebe@4plebs.org>
2016-10-23 14:51:42 +0000

    resurrect external links plugin with archives.json & post expanding

--- bootstrap.php
+++ bootstrap.php
@@ -1,25 +1,25 @@
 <?php
 
-use Foolz\Foolframe\Model\Autoloader;
-use Foolz\Foolframe\Model\Context;
+use Foolz\FoolFrame\Model\Autoloader;
+use Foolz\FoolFrame\Model\Context;
 use Foolz\Plugin\Event;
 
 class HHVM_ExLinks
 {
     public function run()
     {
-        Event::forge('Foolz\Plugin\Plugin::execute.foolz/foolfuuka-plugin-external-links')
+        Event::forge('Foolz\Plugin\Plugin::execute#foolz/foolfuuka-plugin-external-links')
             ->setCall(function ($result) {
                 /** @var Context $context */
                 $context = $result->getParam('context');
                 /** @var Autoloader $autoloader */
                 $autoloader = $context->getService('autoloader');
                 $autoloader->addClassMap([
-                    'Foolz\Foolfuuka\Plugins\ExternalLinks\Model\ExternalLinks' => __DIR__.'/classes/model/external_links.php'
+                    'Foolz\FoolFuuka\Plugins\ExternalLinks\Model\ExternalLinks' => __DIR__.'/classes/model/external_links.php'
                 ]);
 
-                Event::forge('Foolz\Foolfuuka\Model\Comment::processExternalLinks.result.html')
-                    ->setCall('Foolz\Foolfuuka\Plugins\ExternalLinks\Model\ExternalLinks::map')
+                Event::forge('Foolz\FoolFuuka\Model\Comment::processExternalLinks#var.link')
+                    ->setCall('Foolz\FoolFuuka\Plugins\ExternalLinks\Model\ExternalLinks::map')
                     ->setPriority(10);
             });
     }
--- classes/model/external_links.php
+++ classes/model/external_links.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Foolz\Foolfuuka\Plugins\ExternalLinks\Model;
+namespace Foolz\FoolFuuka\Plugins\ExternalLinks\Model;
 
 class ExternalLinks
 {
@@ -9,142 +9,21 @@ class ExternalLinks
         $data = $result->getParam('data');
         $href = $result->getParam('build_href');
 
-        switch ($data->shortname) {
-            case 'warosu':
-                $href['short_link'] = '//fuuka.warosu.org/jp/';
-                $href['query_link'] = '//fuuka.warosu.org/jp/post/'.$data->query.'/';
-                break;
-            case 'rizon':
-                $href['short_link'] = '//qchat.rizon.net';
-                $href['query_link'] = '//qchat.rizon.net/?channels=#'.$data->query;
-                break;
-            case 'irchighway':
-                $href['short_link'] = 'http://webchat.irchighway.net/';
-                $href['query_link'] = 'http://webchat.irchighway.net/?channels=#'.$data->query;
-                break;
-            case 'siztra':
-                $href['short_link'] = '//qchat.rizon.net/?channels=#siztra~';
-                $href['query_link'] = '//qchat.rizon.net/?channels=#siztra~';
-                break;
-
-            case '9fag':
-                $href['short_link'] = '//9gag.com/';
-                $href['query_link'] = '//9gag.com/gag/'.$data->query;
-                break;
-            case 'gaia':
-                $href['short_link'] = 'http://www.gaiaonline.com/profiles/woxxy/8585700/';
-                $href['query_link'] = 'http://www.gaiaonline.com/profiles/woxxy/8585700/';
-                break;
-            case 'moe':
-                $href['short_link'] = 'http://doushio.com/'.$data->shortname.'/';
-                $href['query_link'] = 'http://doushio.com/'.$data->shortname.'/'.$data->query;
-                break;
-            case 'reddit':
-                $href['short_link'] = 'http://reddit.com/'.$data->query;
-                $href['query_link'] = 'http://reddit.com/r/'.$data->query.'/';
-                break;
-            case 'teaparty':
-                $href['short_link'] = 'http://www.seacats.net/'.$data->shortname.'/';
-                $href['query_link'] = 'http://www.seacats.net/'.$data->shortname.'/res/'.$data->query.'.html';
-                break;
-            case 'twitter':
-                $href['short_link'] = '//twitter.com/';
-                $href['query_link'] = '//twitter.com/'.$data->query.'/';
-                break;
-            case 'youtube':
-                $href['short_link'] = '//youtu.be/';
-                $href['query_link'] = '//youtu.be/'.$data->query;
-                $href['attributes'] = ' data-function="youtube" data-video="'.$data->query.'"';
-                break;
-
-            case '3':
-            case 'ck':
-            case 'fa':
-            case 'ic':
-            case 'lit':
-                $href['short_link'] = '//warosu.org/'.$data->shortname.'/';
-                $href['query_link'] = '//warosu.org/'.$data->shortname.'/post/'.$data->query.'/';
-                break;
-
-            case 'cgl':
-            case 'con':
-            case 'g':
-            case 'mu':
-                $href['short_link'] = '//rbt.asia/'.$data->shortname.'/';
-                $href['query_link'] = '//rbt.asia/'.$data->shortname.'/post/'.$data->query.'/';
-                break;
-
-            case 'e':
-            case 'w':
-            case 'wg':
-                $href['short_link'] = '//archive.nyafuu.org/'.$data->shortname.'/';
-                $href['query_link'] = '//archive.nyafuu.org/'.$data->shortname.'/post/'.$data->query.'/';
-                break;
-
-            case 'an':
-            case 'fit':
-            case 'k':
-            case 'mlp':
-            case 'r9k':
-            case 'toy':
-            case 'x':
-                $href['short_link'] = 'http://archive.heinessen.com/'.$data->shortname.'/';
-                $href['query_link'] = 'http://archive.heinessen.com/'.$data->shortname.'/post/'.$data->query.'/';
-                break;
-
-            case 'c':
-            case 'int':
-            case 'out':
-            case 'po':
-            case 'neet':
-                $href['short_link'] = '//archive.thedarkcave.org/'.$data->shortname.'/';
-                $href['query_link'] = '//archive.thedarkcave.org/'.$data->shortname.'/post/'.$data->query.'/';
-                break;
-
-            case 'a':
-            case 'biz':
-            case 'co':
-            case 'diy':
-            case 'gd':
-            case 'jp':
-            case 'm':
-            case 'q':
-            case 'sci':
-            case 'sp':
-            case 'tg':
-            case 'tv':
-            case 'vg':
-            case 'vp':
-            case 'vr':
-            case 'wsg':
-            case 'dev':
-            case 'foolz':
-                $href['short_link'] = '//archive.foolz.us/'.$data->shortname.'/';
-                $href['query_link'] = '//archive.foolz.us/'.$data->shortname.'/post/'.$data->query.'/';
-                break;
-
-            case 'u':
-            case 'kuku':
-                $href['short_link'] = '//nsfw.foolz.us/'.$data->shortname.'/';
-                $href['query_link'] = '//nsfw.foolz.us/'.$data->shortname.'/post/'.$data->query.'/';
-                break;
-
-            case 'beta':
-                $href['short_link'] = '//beta.foolz.us/'.$data->shortname.'/';
-                $href['query_link'] = '//beta.foolz.us/'.$data->shortname.'/post/'.$data->query.'/';
-                break;
-
-            case 'blog':
-                $href['short_link'] = '//blog.foolz.us/';
-                $href['query_link'] = '//blog.foolz.us/'.$data->query.'/';
-                break;
-
-            case 'status':
-                $href['short_link'] = '//status.foolz.us/';
-                $href['query_link'] = '//status.foolz.us/';
-                break;
-        }
-
-        $result->setParam('build_url', $href)->set($href);
+        try {
+            $archives = json_decode(file_get_contents(__DIR__ . '/../../archives.json/archives.json'), true);
+            foreach ($archives as $archive) {
+                if (in_array($data->shortname, $archive['boards'])) {
+                    $href['short_link'] = ($archive['http'] ? 'http' : 'https') . '://' . $archive['domain'] . '/' . $data->shortname . '/';
+                    $href['query_link'] = ($archive['http'] ? 'http' : 'https') . '://' . $archive['domain'] . '/' . $data->shortname . '/post/' . $data->query . '/';
+                    if ($archive['software'] === 'foolfuuka') {
+                        $href['attributes'] = ' class="backlink" target="_blank" data-backlink="true" data-post="' . $data->query . '" data-board="' . $data->shortname . '" data-ffapi="true" data-external="' . $archive['domain'] . '"';
+                    } else {
+                        $href['attributes'] = ' target="_blank"';
+                    }
+                    $result->setParam('build_url', $href)->set($href);
+                    return;
+                }
+            }
+        } catch (\Exception $e) {}
     }
 }
